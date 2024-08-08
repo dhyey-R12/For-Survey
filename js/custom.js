e4b7b61b5fcb65f010d61ec986bb242e66027f31
dhyey-R12 <rajparadhyey12@gmail.com>
2024-08-08 22:30:07 +0530

    Design footer

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,42 @@
-document.querySelector('.theme_btn').addEventListener('click',()=>{
-    if (document.documentElement.getAttribute('data-theme') == 'dark') {
-        document.documentElement.setAttribute('data-theme','light')
-    }
-    else {
-        document.documentElement.setAttribute('data-theme','dark')
-    }
+document.querySelector('.theme_btn').addEventListener('click', () => {
+	if (document.documentElement.getAttribute('data-theme') == 'dark') {
+		document.documentElement.setAttribute('data-theme', 'light')
+	}
+	else {
+		document.documentElement.setAttribute('data-theme', 'dark')
+	}
 })
+
+
+// Accordian JS
+const accordions = document.querySelectorAll(".accordion");
+
+const openAccordion = (accordion) => {
+	const content = accordion.querySelector(".accordion__content");
+	accordion.classList.add("accordion__active");
+	content.style.maxHeight = content.scrollHeight + "px";
+};
+
+const closeAccordion = (accordion) => {
+	const content = accordion.querySelector(".accordion__content");
+	accordion.classList.remove("accordion__active");
+	content.style.maxHeight = null;
+};
+
+accordions.forEach((accordion) => {
+	const intro = accordion.querySelector(".accordion__intro");
+	const content = accordion.querySelector(".accordion__content");
+	const title = accordion.querySelector(".accordion_title");
+	const node = document.createElement("span");
+	
+	title.appendChild(node);
+
+	intro.onclick = () => {
+		if (content.style.maxHeight) {
+			closeAccordion(accordion);
+		} else {
+			accordions.forEach((accordion) => closeAccordion(accordion));
+			openAccordion(accordion);
+		}
+	};
+});
---
+++ notes.md
@@ -0,0 +1,3 @@
+https://codepen.io/ananyaneogi/pen/zXZyMP?editors=0010
+https://theuxspace.studio/home
+https://www.figma.com/design/pknLLsCO6JNEwWLbIOuVTz/For-Survey-(Copy)?t=blylEukgbkB7h22i-0
